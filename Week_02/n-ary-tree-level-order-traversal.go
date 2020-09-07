39b9487b198d03b31d47593bd7ab4943e5af0d2a
vividwei <vividwei@tencent.com>
2020-09-07 23:20:42 +0800

    add n-ary-tree-level-order-traversal.go

---
+++ Week_02/n-ary-tree-level-order-traversal.go
@@ -0,0 +1,34 @@
+// BFS
+// 解题思路：使用队列做, golang没有队列使用切片模拟队列操作
+// cnt 用来记录当前队列长度
+// i 遍历当前队列内容
+// j 遍历孩子节点
+
+func levelOrder(root *Node) [][]int {
+    res := make([][]int, 0)
+    if root == nil {
+        return res
+    }
+
+    queue := make([]*Node, 0)
+    queue = append(queue, root)
+
+    for len(queue) > 0 {
+        cnt := len(queue)
+        ans := make([]int, 0)
+        // 处理一层
+        for i := 0; i < cnt; i++ {
+            // 孩子节点入队
+            for j := 0; j < len(queue[0].Children); j++ {
+                queue = append(queue, queue[0].Children[j])
+            }
+            // 结果输出到层队列里
+            ans = append(ans, queue[0].Val)
+            queue = queue[1:]
+        }
+
+        res = append(res, ans)
+    }
+
+    return res
+}
