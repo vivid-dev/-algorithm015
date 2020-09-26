a6146c129734317083cc907ce581afd5158488f9
vividwei <vividwei@tencent.com>
2020-09-27 00:16:58 +0800

    add lemonade-change.go

---
+++ Week_04/lemonade-change.go
@@ -0,0 +1,34 @@
+func lemonadeChange(bills []int) bool {
+    cp5 := 0
+    cp10 := 0
+
+    flag := true
+    
+    for i := range bills {
+        if bills[i] == 5 {
+            cp5++
+        }
+        if bills[i] == 10 {
+            if cp5 == 0 {
+                flag = false
+                break
+            }
+            cp5--
+            cp10++
+        }
+        if bills[i] == 20 {
+            if cp5 == 0 || (cp10 == 0 && cp5 < 3) {
+                flag = false
+                break
+            }
+            if cp10 == 0 {
+                cp5 -= 3
+            } else {
+                cp5--
+                cp10--
+            }
+        }
+    }
+
+    return flag
+}
